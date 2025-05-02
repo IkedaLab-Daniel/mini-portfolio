fea43d0884c7fa19662310428117700e7d2db4e0
Daniel <callejasmark63@gmail.com>
2025-05-02 11:55:29 +0800

    Initial commit with existing code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import About from './components/About'
+import './index.css'
+
+function App() {
+  return (
+    <main>
+      <About />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,56 @@
+import deviceice from '../assets/deviceice.jpeg'
+import samplePNG from '../assets/sample.png'
+import data from '../stats.json'
+
+function About(){
+    return(
+        <div id="about">
+            <div className="about-container">
+                <div className="left">
+                    <img src={samplePNG} alt="" />
+                    <div className='sidenote'>
+                        <p className="username">@dev.iceice</p>
+                        <p className="caption">Studying Web Development 1 Hour/+ a Day</p>
+                    </div>
+                    
+                </div>
+                <div className="right">
+                    <h1>Studying Web Development 1 Hour/+ a Day</h1>
+                    <div className='tech-stack-container'>
+                        <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5"/>
+                        <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" alt="CSS3"/>
+                        <img src="https://img.shields.io/badge/Bootstrap-7952B3?style=for-the-badge&logo=bootstrap&logoColor=white" alt="Bootstrap"/>
+                        <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript"/>
+                        <img src="https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black" alt="React"/>
+                        <img src="https://img.shields.io/badge/React%20Router-CA4245?style=for-the-badge&logo=reactrouter&logoColor=white" alt="React Router"/>
+                        <img src="https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white" alt="Node.js"/>
+                        <img src="https://img.shields.io/badge/Express.js-303030?style=for-the-badge&logo=express&logoColor=white" alt="Express.js"/>
+                        <img src="https://img.shields.io/badge/Nodemon-76D04B?style=for-the-badge&logo=nodemon&logoColor=white" alt="Nodemon"/>
+                        <img src="https://img.shields.io/badge/JWT-000000?style=for-the-badge&logo=jsonwebtokens&logoColor=white" alt="JWT"/>
+                        <img src="https://img.shields.io/badge/NPM-CB3837?style=for-the-badge&logo=npm&logoColor=white" alt="NPM"/>
+                        <img src="https://img.shields.io/badge/Vite-646CFF?style=for-the-badge&logo=vite&logoColor=white" alt="Vite"/>
+                        <img src="https://img.shields.io/badge/Webpack-8DD6F9?style=for-the-badge&logo=webpack&logoColor=black" alt="Webpack"/>
+                        <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" alt="Python"/>
+                        <img src="https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white" alt="Django"/>
+                        <img src="https://img.shields.io/badge/DRF-ff3d00?style=for-the-badge&logo=django&logoColor=white" alt="Django REST"/>
+                        <img src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white" alt="Postman"/>
+                        <img src="https://img.shields.io/badge/Insomnia-4000BF?style=for-the-badge&logo=insomnia&logoColor=white" alt="Insomnia"/>
+                        <img src="https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white" alt="PHP"/>
+                        <img src="https://img.shields.io/badge/Apache-D22128?style=for-the-badge&logo=apache&logoColor=white" alt="Apache"/>
+                        <img src="https://img.shields.io/badge/MySQL-00758F?style=for-the-badge&logo=mysql&logoColor=white" alt="MySQL"/>
+                        <img src="https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white" alt="MongoDB"/>
+                        <img src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white" alt="Figma"/>
+                        <img src="https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white" alt="Git"/>
+                        <img src="https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white" alt="GitHub"/>
+                    </div>
+                    <div className="stats">
+                        <span>Total Time Spent: <strong>{data['total-time']}</strong> </span>
+                        <span>Steak: <strong>{data['total-days']}</strong></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
